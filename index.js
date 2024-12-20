7aa0be678c9e018944ed37c53453f0c9f9d762a6
SILA <106426620+kongchansila05@users.noreply.github.com>
2024-12-20 12:06:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,18 +7,31 @@ const bot = new TelegramBot(API_KEY, { polling: true });
 
 const REGISTER_API = "https://p-api.sbc369.club/api/cash/registration/";
 const LOGIN_API = "https://p-api.sbc369.club/api/cash/login/";
-const CAPTION =
-    "សំរាប់ចម្ងល់ឬបញ្ហាផ្សេងៗ នឹង ដាក់/ដក ប្រាក់ ចុចទីនេះ 👉🏻 @OUN88  បញ្ជាក់៖ នេះជាម៉ាសុីនសម្រាប់តែបង្កើតអាខោន មិនចេះឆ្លើយតបទេ។ សូមអរគុណ!";
+const CAPTION ="សំរាប់ចម្ងល់ឬបញ្ហាផ្សេងៗ នឹង ដាក់/ដក ប្រាក់ ចុចទីនេះ 👉🏻 @OUN88  បញ្ជាក់៖ នេះជាម៉ាសុីនសម្រាប់តែបង្កើតអាខោន មិនចេះឆ្លើយតបទេ។ សូមអរគុណ!";
 const CERT = "aeR7xc6PTGXPWDZe4YmAP9";
-const IMAGE_URL = "https://i.ibb.co/ZS0XCdd/1637389470-0-promotion-1-1.jpg";
+const IMAGE_welcome = "https://oun88.me/photos/banner_kh/1723705280banner-24-2.jpg";
+const IMAGE_88 = "https://oun88.me/photos/article/1637389470_0_promotion-1.1.jpg";
+const IMAGE_10 = "https://oun88.me/photos/article/1637389513_0_promotion-2_(2).jpg";
+const IMAGE_5 = "https://oun88.me/photos/article/1637389609_0_promotion-3.1.jpg";
 const Authorization = "Token 5868453c13eb70f804b279c97ca4e84a7ef1d14f";
 function generateNineDigitNumber() {
     return Math.floor(100000000 + Math.random() * 900000000);
 }
 
 function handleContactCommand(chatId) {
-    bot.sendPhoto(chatId, IMAGE_URL, { caption: CAPTION });
+    bot.sendPhoto(chatId, IMAGE_welcome, {
+        caption: CAPTION,
+        parse_mode: "HTML", // Use HTML formatting for the caption
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    { text: "👩‍💻ផ្នែកសេវាកម្ម 24/7", url: "https://t.me/oun88" }
+                ]
+            ]
+        }
+    });
 }
+
 bot.onText(/\/(register|start)/, async (msg) => {
     const chatId = msg.chat.id;
     const firstName = msg.from.first_name || "";
@@ -49,17 +62,13 @@ bot.onText(/\/(register|start)/, async (msg) => {
       }).then((response)=>{
         if(response.status == '200'){
             const { domain, sessionid, userid } = response.data;
-            bot.sendMessage(chatId, `គណនីរបស់អ្នក`).then(() => {
-                bot.sendMessage(
-                    chatId,
-                    `ឈ្មោះ​គណនី: \`${FullName}\`\nពាក្យសម្ងាត់: \`${Password}\``,
-                    { parse_mode: "Markdown" }
-                ).then(() => {
-                    const rehref = `${domain}/?sid=${sessionid}&uid=${userid}&cert=${CERT}&language=EN`;
-                    bot.sendMessage(chatId, `Login:\n${rehref}`).then(() => {
-                        handleContactCommand(chatId);
-                    });
-                });
+            const rehref = `${domain}/?sid=${sessionid}&uid=${userid}&cert=${CERT}&language=EN`;
+            bot.sendMessage(
+                chatId,
+                `👤ឈ្មោះ​គណនី: <code>${FullName}</code>\n🔐 លេខសម្ងាត់: <code>${Password}</code>\n🌐 ចូលលេង: <a href="${rehref}">OUN88</a>`,
+                { parse_mode: "HTML" }
+            ).then(() => {
+                handleContactCommand(chatId);
             });
         }
     }).catch(error => {
@@ -72,17 +81,13 @@ bot.onText(/\/(register|start)/, async (msg) => {
                     .then((responseLogin) => {
                         if (responseLogin.status === 200) {
                             const { domain, sessionid, userid } = responseLogin.data;
-                            bot.sendMessage(chatId, `គណនីរបស់អ្នក`).then(() => {
-                                bot.sendMessage(
-                                    chatId,
-                                    `ឈ្មោះ​គណនី: \`${FullName}\`\nពាក្យសម្ងាត់: \`${Password}\``,
-                                    { parse_mode: "Markdown" }
-                                ).then(() => {
-                                    const rehref = `${domain}/?sid=${sessionid}&uid=${userid}&cert=${CERT}&language=EN`;
-                                    bot.sendMessage(chatId, `Login:\n${rehref}`).then(() => {
-                                        handleContactCommand(chatId);
-                                    });
-                                });
+                            const rehref = `${domain}/?sid=${sessionid}&uid=${userid}&cert=${CERT}&language=EN`;
+                            bot.sendMessage(
+                                chatId,
+                                `👤ឈ្មោះ​គណនី: <code>${FullName}</code>\n🔐 លេខសម្ងាត់: <code>${Password}</code>\n🌐 ចូលលេង: <a href="${rehref}">OUN88</a>`,
+                                { parse_mode: "HTML" }
+                            ).then(() => {
+                                handleContactCommand(chatId);
                             });
                         }
                     });
@@ -128,6 +133,68 @@ bot.onText(/\/contact/, (msg) => {
     const chatId = msg.chat.id;
     handleContactCommand(chatId);
 });
+bot.onText(/\/promotion/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 
+        `🌟<b>ការផ្តល់ជូនពិសេស OUN88</b> 🌟\n\n 🎁ស្វាគមន៏សមាជិថ្មី 88%\n 🎁ប្រាក់បន្ថែមរៀងរាល់ថ្ងៃ 10%\n 🎁ប្រាក់បង្វិលប្រចាំខែ 5%`, 
+        {
+            parse_mode: "HTML",
+            reply_markup: {
+                inline_keyboard: [
+                    [{ text: "ស្វាគមន៏សមាជិថ្មី 88%", callback_data: "promo_80" }],
+                    [{ text: "ប្រាក់បន្ថែមរៀងរាល់ថ្ងៃ 10%", callback_data: "promo_10" }],
+                    [{ text: "ប្រាក់បង្វិលប្រចាំខែ 5%", callback_data: "promo_5" }],
+                ]
+            }
+        }
+    );
+    
+});
+
+bot.on('callback_query', (callbackQuery) => {
+    const message = callbackQuery.message;
+    const data = callbackQuery.data;
+    if (data === "promo_80") {
+        bot.sendPhoto(message.chat.id, IMAGE_88, {
+            caption: `🎁ស្វាគមន៏សមាជិថ្មី 80%🧧\n\n - វិលជុំ x7 (ហ្គេមស្លុត)\n - វិលជុំ x13 (ហ្គេមឡាយផ្ទាល់, បាញ់ត្រី)\n - ដាក់ប្រាក់តិចបំផុត $10\n - ដកប្រាក់ធំបំផុត $288`,
+            parse_mode: "HTML",
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        { text: "👩‍💻ផ្នែកសេវាកម្ម 24/7", url: "https://t.me/oun88" }
+                    ]
+                ]
+            }
+        });
+    } else if (data === "promo_10") {
+        bot.sendPhoto(message.chat.id, IMAGE_10, {
+            caption: `🧧ប្រាក់បន្ថែមរៀងរាល់ថ្ងៃ 20%🧧\n\n - វិលជុំ x4 (ហ្គេមស្លុត)\n - វិលជុំ x8 (ហ្គេមឡាយផ្ទាល់, បាញ់ត្រី)\n - ដាក់ប្រាក់តិចបំផុត $10\n - ដកប្រាក់ធំបំផុត $188`,
+            parse_mode: "HTML",
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        { text: "👩‍💻ផ្នែកសេវាកម្ម 24/7", url: "https://t.me/oun88" }
+                    ]
+                ]
+            }
+        });
+    } else if (data === "promo_5") {
+        bot.sendPhoto(message.chat.id, IMAGE_5, {
+            caption: `🧧 ប្រាក់បង្វិលប្រចាំខែ 5%🧧\n\n - វិលជុំ x4 (ហ្គេមស្លុត)\n - វិលជុំ x8 (ហ្គេមឡាយផ្ទាល់, បាញ់ត្រី)\n - ដាក់ប្រាក់តិចបំផុត $10\n - ដកប្រាក់ធំបំផុត $188`,
+            parse_mode: "HTML",
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        { text: "👩‍💻ផ្នែកសេវាកម្ម 24/7", url: "https://t.me/oun88" }
+                    ]
+                ]
+            }
+        });
+    } else if (data === "register") {
+        bot.sendMessage(message.chat.id, "You clicked on Register Now!");
+    } 
+});
+
 
 const express = require("express");
 const { log } = require("console");
